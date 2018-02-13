fb870e0d5b73480a9f7a8369fbf71fae1df69328
Steven Pray <steven@stevenpray.com>
2018-02-13 09:06:16 -0500

    run client test in travis

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,28 +1,23 @@
+import {APP_BASE_HREF} from '@angular/common';
 import {async, TestBed} from '@angular/core/testing';
 import {AppComponent} from './app.component';
+import {AppModule} from './app.module';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [
-                AppComponent,
+            imports: [
+                AppModule,
+            ],
+            providers: [
+                {provide: APP_BASE_HREF, useValue: '/'},
             ],
         }).compileComponents();
     }));
+
     it('should create the app', async(() => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
-    }));
-    it(`should have as title 'Symfony-Angular Starter'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('Symfony-Angular Starter');
-    }));
-    it('should render title in a h1 tag', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
     }));
 });
